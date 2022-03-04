3297a2dd0cd8d941004aba6847d913ae1d972c24
Christian <christian.jhardy.steinmann@gmail.com>
2022-03-04 23:21:12 +0200

    TokenSale

--- contracts/Steinnegen.sol
+++ contracts/Steinnegen.sol
@@ -3,8 +3,17 @@ pragma solidity >=0.4.22 <0.9.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract Steinnegen is ERC20 {
-
-  constructor() ERC20("Steinnegen", "STEIN") {
+    constructor() ERC20("Steinnegen", "STEIN") {
         _mint(msg.sender, 100000000000000000000000000);
-    }    
+    }
+
+    uint256 public totalWeiRaised;
+    uint256 public tokensMinted;
+    uint256 public totalSupply;
+    uint256 public contributors;
+    uint256 public decimalsMultiplier;
+    uint256 public startTime;
+    uint256 public endTime;
+    uint256 public remainingTokens;
+    uint256 public allocatedTokens;
 }
