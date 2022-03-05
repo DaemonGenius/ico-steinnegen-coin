9dc1852443cea50d4587eb4efee1288d48870bf7
Christian <christian.jhardy.steinmann@gmail.com>
2022-03-05 19:32:31 +0200

    Extended Transfer features

--- contracts/Steinnegen.sol
+++ contracts/Steinnegen.sol
@@ -3,8 +3,27 @@ pragma solidity >=0.4.22 <0.9.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract Steinnegen is ERC20 {
+    
+    bool public transfersEnabled;
+    bool public masterTransfersEnabled;
+
     constructor() ERC20("Steinnegen", "STEIN") {
         _mint(msg.sender, 100000000000000000000000000);
     }
 
+    /**
+     * Enable or block transfers - to be called in case of emergency
+     * @param _value {bool}
+     */
+    function enableTransfers(bool _value) public {
+        transfersEnabled = _value;
+    }
+
+    /**
+     * Enable or block transfers - to be called in case of emergency
+     * @param _value {bool}
+     */
+    function enableMasterTransfers(bool _value) public {
+        masterTransfersEnabled = _value;
+    }
 }
