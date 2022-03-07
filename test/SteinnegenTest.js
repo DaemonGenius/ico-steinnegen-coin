8f5c082197dd7c77234941455b8fc1e2fc75f0a6
Christian <christian.jhardy.steinmann@gmail.com>
2022-03-07 14:25:45 +0200

    Fixing contract

--- test/SteinnegenTest.js
+++ test/SteinnegenTest.js
@@ -105,36 +105,6 @@ contract("Coin", async function (accounts) {
     );
   });
 
-  // it('transfers token ownership', function() {
-  //   return Coin.deployed().then(function(instance) {
-  //     tokenInstance = instance;
-  //     // Test `require` statement first by transferring something larger than the sender's balance
-  //     return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
-  //   }).then(assert.fail).catch(function(error) {
-  //     console.log(error);
-  //     assert(error.message.indexOf('overflow') >= 0, 'error message must contain revert');
-  //     // This will only call the function, and not write to disk
-  //     // We want to inspect the function return value rather than the transaction receipt
-  //     return tokenInstance.transfer.call(accounts[1], 250000);
-  //   }).then(function(success) {
-  //     assert.equal(success, true, 'it returns true')
-  //     // Actually transfer by calling the function & writing to disk
-  //     return tokenInstance.transfer(accounts[1], 250000);
-  //   }).then(function(receipt) {
-  //     assert.equal(receipt.logs.length, 1, 'triggers one event');
-  //     assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
-  //     assert.equal(receipt.logs[0].args.from, accounts[0], 'logs the account the tokens are transferred from');
-  //     assert.equal(receipt.logs[0].args.to, accounts[1], 'logs the account the tokens are transferred to');
-  //     assert.equal(receipt.logs[0].args.value, 250000, 'logs the transfer amount');
-  //     return tokenInstance.balanceOf(accounts[0]);
-  //   }).then(function(balance) {
-  //     assert.equal(balance.toNumber(), 1999999999750000, 'deducts the amount from the sending account');
-  //     return tokenInstance.balanceOf(accounts[1]);
-  //   }).then(function(balance) {
-  //     assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
-  //   })
-  // });
-
   it("approves tokens for delegated transfer", async () => {
     let approve = await coinInstance.approve.call(accounts[1], 100);
     assert.equal(approve, true, "Success");
